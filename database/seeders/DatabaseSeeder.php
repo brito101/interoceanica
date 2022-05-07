fdd3f5f8a4d89702b7ccd125ef09470223bae071
brito101 <rodrigobrito101@hotmail.com>
2022-05-07 11:24:20 -0300

    Finish seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
             RolesTableSeeder::class,
             RolesHasPermissionTableSeeder::class,
             ModelHasRoleTableSeeder::class,
+            RastImpoTableSeeder::class,
+            RastExpoTableSeeder::class,
         ]);
     }
 }
