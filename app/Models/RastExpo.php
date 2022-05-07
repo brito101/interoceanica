6dd9b1b9684518b8f1336e358f8f87fea14bdfb7
brito101 <rodrigobrito101@hotmail.com>
2022-05-07 12:23:06 -0300

    Create user teste

--- app/Models/RastExpo.php
+++ app/Models/RastExpo.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class RastExpo extends Model
 {
     use HasFactory;
+
+    protected $table = 'Rast_Expo';
 }
--- app/Models/RastImpo.php
+++ app/Models/RastImpo.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class RastImpo extends Model
 {
     use HasFactory;
+
+    protected $table = 'Rast_Expo';
 }
